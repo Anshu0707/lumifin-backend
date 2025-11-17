bea1b130b5940ee869b35f2d1eaf9924ef5a0bb7
Anshuman Naithani <anshumannaithani7@gmail.com>
2025-11-17 16:59:21 +0530

    Add middleware file

---
+++ user-creation/middlewares/userValidation.js
@@ -0,0 +1,21 @@
+export default function userValidation(req, res, next) {
+  const { name, email } = req.body;
+
+  if (!name || !email) {
+    return res.status(400).json({
+      success: false,
+      message: "Name and Email are required",
+    });
+  }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid email format",
+    });
+  }
+
+  next();
+}
--- user-creation/routes/userRoutes.js
+++ user-creation/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { createUser } from "../controllers/userController.js";
+import userValidation from "../middlewares/userValidation.js";
 
 const router = Router();
 
-router.post("/create", createUser);
+// Middleware to validate user data before creating a new user
+router.post("/create", userValidation, createUser);
 
 export default router;
